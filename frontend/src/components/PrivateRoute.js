8fde41df4fd3ee896554e034bbd896f4e7d3ca37
PSS0702 <aaaqkrtmd@gmail.com>
2024-09-30 21:21:55 +0900

    Create PrivateRoute.js

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+
+function PrivateRoute({ component: Component, ...rest }) {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/auth", state: { from: props.location } }} />
+        )
+      }
+    />
+  );
+}
+
+export default PrivateRoute;
