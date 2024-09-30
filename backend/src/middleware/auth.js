a6a479309303fa32114ee9994f237af55c2036e8
PSS0702 <aaaqkrtmd@gmail.com>
2024-09-30 21:09:35 +0900

    Create auth.js

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header('Authorization');
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token.split(' ')[1], config.jwtSecret);
+    
+    // Add user from payload
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
