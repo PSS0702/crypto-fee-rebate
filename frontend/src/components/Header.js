7e6a33cc38bb516f3e0bb570e1720a7c7b314d72
PSS0702 <aaaqkrtmd@gmail.com>
2024-09-30 20:53:45 +0900

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header>
-      <h1>Crypto Fee Rebate</h1>
-      {/* Add navigation menu items here */}
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/exchanges">Exchanges</Link></li>
+          <li><Link to="/calculator">Calculator</Link></li>
+          <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/auth">Login/Register</Link></li>
+        </ul>
+      </nav>
     </header>
   );
 }
