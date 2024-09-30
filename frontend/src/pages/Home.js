b59d47bcfe94e5ad4b4d204256d8863be84a852a
PSS0702 <aaaqkrtmd@gmail.com>
2024-09-30 20:41:51 +0900

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Crypto Fee Rebate</h1>
+      <p>Find the best rebates for your crypto trades!</p>
+    </div>
+  );
+}
+
+export default Home;
