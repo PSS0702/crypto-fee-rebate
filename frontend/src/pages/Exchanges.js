50fa34b1d586f5ba4eb1d7abf691f0e1f304253a
PSS0702 <aaaqkrtmd@gmail.com>
2024-09-30 20:42:04 +0900

    Update Exchanges.js

--- frontend/src/pages/Exchanges.js
+++ frontend/src/pages/Exchanges.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function Exchanges() {
+  return (
+    <div>
+      <h2>Supported Exchanges</h2>
+      {/* Add list of exchanges here */}
+    </div>
+  );
+}
+
+export default Exchanges;
