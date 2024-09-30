803f774a4abeabb5e02fd6744122947df04ec5dd
PSS0702 <aaaqkrtmd@gmail.com>
2024-09-30 21:17:34 +0900

    Update ErrorMessage.js

--- frontend/src/components/ErrorMessage.js
+++ frontend/src/components/ErrorMessage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Typography, Paper } from '@material-ui/core';
 
 function ErrorMessage({ message }) {
   return (
-    <div className="error-message">
-      <p>{message}</p>
-    </div>
+    <Paper style={{ padding: '20px', margin: '20px', backgroundColor: '#ffebee' }}>
+      <Typography color="error">{message}</Typography>
+    </Paper>
   );
 }
 
