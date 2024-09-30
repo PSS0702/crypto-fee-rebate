c92477a1f33490af7ae9372410afbba8d722a5db
PSS0702 <aaaqkrtmd@gmail.com>
2024-09-30 20:38:07 +0900

    Add Header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Crypto Fee Rebate</h1>
+      {/* Add navigation menu items here */}
+    </header>
+  );
+}
+
+export default Header;
