1f85ebc860907c2087f51bb5c1c36c5fcd1a4e24
PSS0702 <aaaqkrtmd@gmail.com>
2024-09-30 20:42:17 +0900

    Update Calculator.js

--- frontend/src/pages/Calculator.js
+++ frontend/src/pages/Calculator.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function Calculator() {
+  return (
+    <div>
+      <h2>Rebate Calculator</h2>
+      {/* Add calculator form here */}
+    </div>
+  );
+}
+
+export default Calculator;
