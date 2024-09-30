b3852f252fce80d4d5cc89c4a7678608d69baff3
PSS0702 <aaaqkrtmd@gmail.com>
2024-09-30 21:16:52 +0900

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/exchanges">Exchanges</Link></li>
-          <li><Link to="/calculator">Calculator</Link></li>
-          <li><Link to="/dashboard">Dashboard</Link></li>
-          <li><Link to="/auth">Login/Register</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          Crypto Fee Rebate
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/exchanges">Exchanges</Button>
+        <Button color="inherit" component={Link} to="/calculator">Calculator</Button>
+        <Button color="inherit" component={Link} to="/dashboard">Dashboard</Button>
+        <Button color="inherit" component={Link} to="/auth">Login/Register</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
