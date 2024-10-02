37ecf9fb1a78d8b5206e4fe5b0cf51cbd3df5f3a
PSS0702 <aaaqkrtmd@gmail.com>
2024-10-02 13:43:46 +0900

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,10 @@
-// import KakaoLogin from 'react-kakao-login';  // 이 줄을 주석 처리
 import React from 'react';
 
 function Login() {
   return (
     <div>
       <h2>Login Page</h2>
-      {/* Kakao 로그인 관련 코드도 주석 처리 */}
+      {/* Kakao 로그인 기능은 추후 구현 예정 */}
     </div>
   );
 }
